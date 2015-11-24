406026f0f44396a9956d501ef0e28837bf71ed8a
Volodymyr Shatsky <shockone89@gmail.com>
2015-11-24 21:00:53 +0200

    Add printOptions.

--- app/Main.hs
+++ app/Main.hs
@@ -4,5 +4,5 @@ import Lib
 
 main :: IO ()
 main = do
-    chosen <- oneOf ["first", "second", "third"]
+    chosen <- oneOf ["first", "second", "third", "sdflkjsdlfjksldkjf", "sssssssssssssssssssssssssssssssss", "oiuuuuuuuuuuuuuuuuuuuuuuuuuuwlkjs"]
     print chosen
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,8 +7,15 @@ import Data.Text.Format
 import System.Console.Terminal.Size
 import Data.Maybe (fromMaybe)
 
-type Current = Bool
-data InteractivelySelectable a => Line a = Line a Current
+type IsCurrent = Bool
+
+data InteractivelySelectable a => Line a = Line a IsCurrent
+data InteractivelySelectable a => Options a = Options { getAboveScreen :: [Line a]
+                                                      , getAboveCurrent :: [Line a] -- FIXME: have a VisibleOptions newtype after changing [] to Traversable.
+                                                      , getCurrent :: Line a
+                                                      , getBelowCurrent :: [Line a]
+                                                      , getBelowScreen :: [Line a]
+                                                      }
 
 class InteractivelySelectable a where
     showOption :: a -> String
@@ -17,20 +24,40 @@ instance InteractivelySelectable String where
     showOption = id
 
 oneOf :: InteractivelySelectable options => [options] -> IO String
-oneOf allLines@(firstLine:restOfLines) = do
+oneOf lines = do
     handle <- tty
+
     hSetBuffering handle NoBuffering
     hSetEcho handle False
 
-    mapM_ printLine $ Line firstLine True : map (`Line` False) restOfLines
-    hCursorUpLine handle (length allLines)
+    buildOptions lines <$> getTerminalHeight handle >>= printOptions
+    hCursorUpLine handle (length lines)
 
     char <- hGetChar handle
+    hClose handle
     return.show $ char
+
+
+buildOptions :: (InteractivelySelectable option) => [option] -> Int -> Options option
+buildOptions [] _ = undefined -- FIXME: handle this case properly.
+buildOptions options@(headOption:tailOptions) terminalHeight =
+    Options [] [] current (map toNotCurrentLine (take belowLength tailOptions)) (map toNotCurrentLine (drop belowLength tailOptions))
+    where current = Line headOption True
+          belowLength | length options <= terminalHeight = length options - 1
+                      | otherwise                        = terminalHeight - 1
+          toNotCurrentLine = (`Line` False)
 
 
 tty :: IO Handle
 tty = openFile "/dev/tty" ReadWriteMode
+
+
+-- FIXME: Rewrite after adding VisibleOptions.
+printOptions :: InteractivelySelectable a => Options a -> IO ()
+printOptions options = do
+    mapM_ printLine (getAboveCurrent options)
+    printLine (getCurrent options)
+    mapM_ printLine (getBelowCurrent options)
 
 
 printLine :: InteractivelySelectable a => Line a -> IO ()
@@ -41,7 +68,7 @@ printLine (Line option isCurrent) = do
     hprint handle "{}\n" $ Only paddedString
 
 
-sgr :: Current -> SGR
+sgr :: IsCurrent -> SGR
 sgr True = SetSwapForegroundBackground True
 sgr False = Reset
 
@@ -50,6 +77,14 @@ pad handle string = do
     return (right terminalWidth ' ' string)
 
 
-getTerminalWidth :: Integral n => Handle -> IO n
+getTerminalWidth :: Handle -> IO Int
 getTerminalWidth handle = extractWidth <$> hSize handle
-    where extractWidth = width . fromMaybe (Window 24 80)
+    where extractWidth = width . fromMaybe defaultTerminalWindow
+
+
+getTerminalHeight :: Handle -> IO Int
+getTerminalHeight handle = extractHeight <$> hSize handle
+    where extractHeight = height . fromMaybe defaultTerminalWindow
+
+
+defaultTerminalWindow = Window 24 80
