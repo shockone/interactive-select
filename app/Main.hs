56b918cd217e606250b4d8763e3d6f3c7e59676f
Volodymyr Shatsky <shockone89@gmail.com>
2015-11-27 18:21:59 +0200

    Implement command line -m switch.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
+import Options.Applicative
 import Lib
 
+data Mode = One | Many
+
 main :: IO ()
-main = getContents >>= manyOf . lines >>= mapM_ putStrLn
+main = do
+    mode <- execParser parserInfo
+    fmap lines getContents >>= select mode
+
+
+parserInfo :: ParserInfo Mode
+parserInfo = info (helper <*> parser) description
+    where parser      = flag One Many $ long "many" <> short 'm' <> help "Select multiple options."
+          description = fullDesc <> progDesc "Interactively select from whatever is passed to STDIN"
+
+
+select :: Mode -> [String] -> IO ()
+select One rows = oneOf rows >>= putStrLn
+select Many rows = manyOf rows >>= mapM_ putStrLn
